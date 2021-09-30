1e43c68e3ec91f304ffd78bde403ec9723cd0c01
monicaLozano <monicalt21@hotmail.com>
2021-09-30 14:48:56 -0700

    imprima el tamaño de varios tipos en la computadora en la que se compila y ejecuta.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 /**
  * main - imprimir en C with printf
- *int inttype
- *float floatType
- *double doubletype
- *char charType
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+int i;
+char c;
+long int l;
+long long int ll;
+float f;  
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
 printf("Size of double: %zu bytes\n", sizeof(doubleType));
