305a01ca558e15d4437128fe1b92025ff0845816
monicaLozano <monicalt21@hotmail.com>
2021-10-01 12:11:58 -0700

    completar numero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,13 +10,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-printf("Enter a number: \n");
-scanf("%d", &num);
 if (num > 0)
 printf("%d is a positive number \n", num);
 else if (num < 0)
 printf("%d is a negative number \n", num);
 else
-printf("0 is neither positive nor negative");
+printf("is zero");
 return (0);
 }
