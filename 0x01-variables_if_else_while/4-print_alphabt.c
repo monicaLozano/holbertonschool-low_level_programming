b03206f6fd6d46e7213710ff3190e5fcc5173c48
monicaLozano <monicalt21@hotmail.com>
2021-10-01 13:46:55 -0700

    imprimit alfabet sin la q y la e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabeth in lowercase. except q and e
+ *
+ *
+ * Return: always zero.
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+	  if (i != 'e' && i != 'q')
+	    {
+	      putchar(i);
+	      putchar('\n');
+	    }
+	}
+	return (0);
+}
