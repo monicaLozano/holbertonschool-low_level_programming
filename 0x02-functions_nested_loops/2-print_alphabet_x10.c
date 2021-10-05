92bdfd31c49ade9087b2d3f09b69b44202978b13
monicaLozano <monicalt21@hotmail.com>
2021-10-05 09:48:29 -0700

    10 print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,9 +3,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* 
-* print_alphabet_x10 - t prints 10 times the alphabet, in lowercase
-* 
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+*
 */
 void print_alphabet_x10(void)
 {
