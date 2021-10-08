51a3e322147a3810839eeb5fce552e0d859a84a9
monicaLozano <monicalt21@hotmail.com>
2021-10-08 08:49:34 -0700

    isuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code.
- *
+ * _isupper - checks for uppercase character
+ * @c: character
  * Return: Always 0.
  */
 int _isupper(int c)
