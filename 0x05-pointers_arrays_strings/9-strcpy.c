c01b3f5e8ae431841822ab3d76827db802f8af15
monicaLozano <monicalt21@hotmail.com>
2021-10-11 21:20:11 -0700

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,11 +10,22 @@
  */
 char *_strcpy(char *dest, char *src)
 {
+	int control = 0;
+
 	int temp;
 
 	char *aux = dest;
 
 	for (temp = 0; *src + temp <= '\0'; temp++)
-		*dest++ = *src++;
+	{
+		if (*src + temp > '\0')
+		{
+			control = 1;
+		}
+		if (control == 0)
+		{
+			*dest++ = *src++;
+		}
+	}
 	return (aux);
 }
